2a0a5f2c899f8c702dc70427c4216255349d9e90
LinkTed <link.ted@mailbox.org>
2020-12-04 16:44:05 +0100

    Updated dependencies

--- src/connection/list_path.rs
+++ src/connection/list_path.rs
@@ -1,27 +1,16 @@
 use super::connection::Connection;
+use dbus_message_parser::ObjectPath;
 use futures::channel::oneshot::Sender;
 use std::collections::HashSet;
-use std::ops::Deref;
 
 impl Connection {
-    pub(super) fn list_path(&mut self, path: &str, sender: Sender<HashSet<String>>) {
+    pub(super) fn list_path(&mut self, object_path: &ObjectPath, sender: Sender<HashSet<String>>) {
         // List the handler.
         let mut result = HashSet::new();
 
-        let path_with_slash = if path == "/" {
-            path.to_string()
-        } else {
-            format!("{}/", path)
-        };
-
-        let split_at = path_with_slash.len();
         for p in self.method_calls.keys() {
-            if p.deref() == path {
-                continue;
-            } else if p.starts_with(&path_with_slash) {
-                let (_, last) = p.split_at(split_at);
-                let base: Vec<&str> = last.splitn(2, '/').collect();
-                if let Some(base) = base.get(0) {
+            if let Some(mut split) = p.strip_prefix_elements(&object_path) {
+                if let Some(base) = split.next() {
                     result.insert(base.to_string());
                 }
             }
